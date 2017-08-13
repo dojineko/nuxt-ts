dc011989a1d35e52b4552e883526d8c2a2d95b89
dojineko <dojineko.spiral@gmail.com>
2017-08-13 10:49:34 +0900

    Update sample page

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>Hello World</div>
+  <div>Hello, World!</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'nuxt-class-component'
+// import { Inject, Model, Prop, Provide, Watch } from 'vue-property-decorator'
 
-@Component({})
+@Component()
 export default class extends Vue {
 }
 </script>
+
+<style module>
+</style>
