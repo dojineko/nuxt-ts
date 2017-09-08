4b2afe03c9f1ca2e7a23471090756b1a9b12c4d5
dojineko <dojineko.spiral@gmail.com>
2017-09-09 00:13:24 +0900

    Update typescript module

--- template/modules/typescript.ts
+++ template/modules/typescript.ts
@@ -1,15 +1,12 @@
 module.exports = function(options, next) {
   this.extendBuild((config, context) => {
+    const vueLoader = config.module.rules.find((rule) => rule.loader === 'vue-loader')
+    vueLoader.options.loaders.ts = 'ts-loader?{"appendTsSuffixTo":["\\\\.vue$"]}!tslint-loader?{"emitErrors":true}'
+
     config.module.rules.push({
       loader: 'ts-loader!tslint-loader?{"emitErrors":true}',
       test: /\.ts$/,
     })
-
-    for (const rule of config.module.rules) {
-      if (rule.loader === 'vue-loader') {
-        rule.query.loaders.ts = 'ts-loader?{"appendTsSuffixTo":["\\\\.vue$"]}!tslint-loader?{"emitErrors":true}'
-      }
-    }
   })
 
   next()
